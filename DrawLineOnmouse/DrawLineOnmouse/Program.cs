a0843b53af665b3997ed30f8d3b74cb158dd674d
arif1613 <arif1613@yahoo.com>
2016-02-22 17:26:29 +0100

    Some refactoring

--- DrawLineOnmouse/DrawLineOnmouse/Program.cs
+++ DrawLineOnmouse/DrawLineOnmouse/Program.cs
@@ -19,8 +19,8 @@ namespace DrawLineOnmouse
 			Application.Run(new Form1());
 		}
 
-		private Point _setPointA, _setPointB;
-		private static List<Point> PointList = new List<Point>();
+		//private Point _setPointA, _setPointB;
+		private static IList<Point> CurrentPointsList = new List<Point>();
 		private Dictionary<Point, Point> PointDictionary = new Dictionary<Point, Point>();
 
 		public Form1()
@@ -32,27 +32,25 @@ namespace DrawLineOnmouse
 		{
 			if (e.Button == MouseButtons.Left)
 			{
-				if (!PointList.Any())
+				if (!CurrentPointsList.Any())
 				{
-					_setPointA = e.Location;
-					PointList.Add(_setPointA);
+					//add selected point to current points list
+					AddPointsToCurrentList(e.Location);
 				}
 				else
 				{
-					_setPointB = e.Location;
-					PointList.Add(_setPointB);
-
+					//add selected point to current points list
+					AddPointsToCurrentList(e.Location);
 					//check if not intersects
-
-					if (_setPointA.X <= _setPointB.X)
+					if (!IsIntersects(CurrentPointsList[0], CurrentPointsList[1]))
 					{
-						PointDictionary.Add(_setPointA, _setPointB);
+						PointDictionary.Add(CurrentPointsList[0], CurrentPointsList[1]);
+						Refresh();
 					}
 					else
 					{
-						PointDictionary.Add(_setPointB, _setPointA);
+						CurrentPointsList.Clear();
 					}
-					Refresh();
 				}
 			}
 			base.OnMouseClick(e);
@@ -65,18 +63,81 @@ namespace DrawLineOnmouse
 			}
 
 			base.OnPaint(e);
-			PointList.Clear();
+			CurrentPointsList.Clear();
+		}
+
+		private void AddPointsToCurrentList(Point PointClicked)
+		{
+			CurrentPointsList.Add(PointClicked);
 		}
 
 
-		//public bool IsIntersects(Point a, Point b)
-		//{
-		//	int x1, y1, x2, y2;
-		//	x1 = 10;
-		//	y1 = 10;
-		//	x2 = 85;
-		//	y2 = 85;
-		//}
+
+		public bool IsIntersects(Point a, Point b)
+		{
+
+			List<Point> allPointsList = GetAllPointsForPreviousDrawnLines();
+
+
+			//checking if any point of the list is on the given line
+
+			var m1 = GetSlopOfTheLine(a.X, a.Y, b.X, b.Y);
+			int uppervalue, lowervalue;
+			//if (a.X>=b.X)
+			//{
+			//	u
+			//}
+			
+			for (int j = a.X; j < b.X; j++)
+			{
+				var point = new Point() { X = j, Y = Convert.ToInt32(m1 * j) };
+				if (allPointsList.Contains(point))
+				{
+					return true;
+				}
+
+			}
+
+			for (int j = a.Y; j < b.Y; j++)
+			{
+				var point = new Point() { X = j, Y = Convert.ToInt32(m1 * j) };
+				if (allPointsList.Contains(point))
+				{
+					return true;
+				}
+
+			}
+			return false;
+		}
+
+		private double GetSlopOfTheLine(int x1, int y1, int x2, int y2)
+		{
+			if ((x2 - x1) == 0) return 0;
+			return (double)(y2 - y1)/(x2 - x1);
+		}
+
+		private List<Point> GetAllPointsForPreviousDrawnLines()
+		{
+			var pointlist = new List<Point>();
+			int x1, y1, x2, y2;
+			x1 = 10;
+			y1 = 10;
+			x2 = 85;
+			y2 = 85;
+
+			var m = GetSlopOfTheLine(x1, y1, x2, y2);
+			for (int i = x1; i < x2; i++)
+			{
+
+				pointlist.Add(new Point
+				{
+					X = i,
+					Y = Convert.ToInt32(m * i)
+				});
+			}
+
+			return pointlist;
+		}
 	}
 
 }
