03a093f4b293124dd8b961e19085aad9c8fe8b6d
arif1613 <arif1613@yahoo.com>
2016-02-25 10:07:39 +0100

    GpsGate projects solved partially

--- GpsGate/GpsGate/GpsGate/LineEquation.cs
+++ GpsGate/GpsGate/GpsGate/LineEquation.cs
@@ -13,15 +13,9 @@ namespace GpsGate
 		public Point End { get; set; }
 		public Point OtherLIneStart { get; set; }
 		public Point OtherLIneEnd { get; set; }
-
 		public int LengthOfClickedLine { get; set; }
 		public int LengthOfPreviousLine { get; set; }
 
-		//public double B1{ get; set; }
-		//public double C1 { get; set; }
-		//public double A2 { get; set; }
-		//public double B2 { get; set; }
-		//public double C2 { get; set; }
 		public LineEquation(Point x1, Point y1, Point x2, Point y2)
 		{
 			Start = x1;
@@ -40,26 +34,62 @@ namespace GpsGate
 			var mPreviousline = GetLineEquationConstants(OtherLIneStart, OtherLIneEnd)[0];
 			var cPreviousline = GetLineEquationConstants(OtherLIneStart, OtherLIneEnd)[1];
 			var allPoints = new List<Point>();
+		
+
 			if (LengthOfClickedLine > LengthOfPreviousLine)
 			{
-				allPoints = GetAllPointsForDrawnLines(Start, End);
 
-				if (allPoints.Select(p => p.Y - (mPreviousline * p.X) + cPreviousline).Any(result => (double)(result) == 0.0))
+				for (int i = Start.X; i < End.X; i++)
 				{
-					return true;
+					if (Start.Y<End.Y)
+					{
+						for (int j = Start.Y; j < End.Y; j++)
+						{
+							if (((int)(j - (mClickedline * i) - cClickedline) - (int)(j - (mPreviousline * i) - cPreviousline)) == 0)
+							{
+								return true;
+							}
+						}
+					}
+					else
+					{
+						for (int j = End.Y; j < Start.Y; j++)
+						{
+							if (((int)(j - (mClickedline * i) - cClickedline) - (int)(j - (mPreviousline * i) - cPreviousline)) == 0)
+							{
+								return true;
+							}
+						}
+					}
 				}
-				return allPoints.Select(p => p.X - (mPreviousline*p.Y) - cPreviousline).Any(result => Convert.ToInt32(result) == 0);
 			}
 			else
 			{
-				allPoints = GetAllPointsForDrawnLines(OtherLIneStart, OtherLIneEnd);
-				if (allPoints.Select(p => p.Y - (mClickedline * p.X) + cClickedline).Any(result => Convert.ToInt32(result) == 0))
+				for (int i = OtherLIneStart.X; i < OtherLIneEnd.X; i++)
 				{
-					return true;
+					if (OtherLIneStart.Y < OtherLIneEnd.Y)
+					{
+						for (int j = OtherLIneStart.Y; j < OtherLIneEnd.Y; j++)
+						{
+							if (((int)(j - (mClickedline * i) - cClickedline) - (int)(j - (mPreviousline * i) - cPreviousline)) == 0)
+							{
+								return true;
+							}
+						}
+					}
+					else
+					{
+						for (int j = OtherLIneEnd.Y; j < OtherLIneStart.Y; j++)
+						{
+							if (((int)(j - (mClickedline * i) - cClickedline) - (int)(j - (mPreviousline * i) - cPreviousline)) == 0)
+							{
+								return true;
+							}
+						}
+					}
 				}
-				return allPoints.Select(p => p.X - (mClickedline*p.Y) - cClickedline).Any(result => Convert.ToInt32(result) == 0);
 			}
-
+			return false;
 		}
 
 		private float[] GetLineEquationConstants(Point clickedPoint1, Point clickedPoint2)
@@ -73,58 +103,60 @@ namespace GpsGate
 			constantArray[1] = c;
 			return constantArray;
 		}
-		public List<Point> GetAllPointsForDrawnLines(Point p, Point q)
-		{
-			var pointlist = new List<Point>();
-			var lineConstantValues = GetLineEquationConstants(new Point { X = p.X, Y = p.Y }, new Point { X = q.X, Y = q.Y });
-			var m = lineConstantValues[0];
-			var c = lineConstantValues[1];
-			if (p.X < q.X)
-			{
-				for (int i = p.X; i <= q.X; i++)
-				{
-					var y = m * i + c;
-					int y1;
-					try
-					{
-						y1 = Convert.ToInt32(y);
-					}
-					catch (Exception e)
-					{
-						y1 = 1;
-					}
-					pointlist.Add(new Point
-					{
-						X = i,
-						Y = y1
-					});
-				}
+		//public List<Point> GetAllPointsForDrawnLines(Point p, Point q)
+		//{
+		//	var pointlist = new List<Point>();
+		//	var lineConstantValues = GetLineEquationConstants(new Point { X = p.X, Y = p.Y }, new Point { X = q.X, Y = q.Y });
+		//	var m = lineConstantValues[0];
+		//	var c = lineConstantValues[1];
+		//	if (p.X < q.X)
+		//	{
+		//		for (int i = p.X; i <= q.X; i++)
+		//		{
+		//			var y = m * i + c;
+		//			int y1;
+		//			try
+		//			{
+		//				y1 = Convert.ToInt32(y);
+		//			}
+		//			catch (Exception e)
+		//			{
+		//				y1 = 1;
+		//			}
+		//			pointlist.Add(new Point
+		//			{
+		//				X = i,
+		//				Y = y1
+		//			});
+		//		}
 
-			}
-			else
-			{
-				for (int i = q.X; i <= p.X; i++)
-				{
-					var y = m * i + c;
-					int y1;
-					try
-					{
-						y1 = Convert.ToInt32(y);
-					}
-					catch (Exception e)
-					{
-						y1 = 1;
-					}
-					pointlist.Add(new Point
-					{
-						X = i,
-						Y = y1
-					});
-				}
-			}
-			return pointlist.OrderByDescending(r => r.X).ToList();
-		}
+		//	}
+		//	else
+		//	{
+		//		for (int i = q.X; i <= p.X; i++)
+		//		{
+		//			var y = m * i + c;
+		//			int y1;
+		//			try
+		//			{
+		//				y1 = Convert.ToInt32(y);
+		//			}
+		//			catch (Exception e)
+		//			{
+		//				y1 = 1;
+		//			}
+		//			pointlist.Add(new Point
+		//			{
+		//				X = i,
+		//				Y = y1
+		//			});
+		//		}
+		//	}
+		//	return pointlist.OrderByDescending(r => r.X).ToList();
+		//}
 
+
+		
 	}
 
 }
